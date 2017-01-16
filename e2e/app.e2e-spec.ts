002378e305eab0cccf102aa6d631095d997a343e
angular-cli <angular-cli@angular.io>
2017-01-16 09:19:22 +0800

    chore: initial commit from angular-cli                           _                      _ _    __ _ _ __   __ _ _   _| | __ _ _ __       ___| (_)   / _  |  _ \ / _  | | | | |/ _  |  __|____ / __| | |  | (_| | | | | (_| | |_| | | (_| | | |_____| (__| | |   \____|_| |_|\__  |\____|_|\____|_|        \___|_|_|               |___/

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { WelinktalentClientPage } from './app.po';
+
+describe('welinktalent-client App', function() {
+  let page: WelinktalentClientPage;
+
+  beforeEach(() => {
+    page = new WelinktalentClientPage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
