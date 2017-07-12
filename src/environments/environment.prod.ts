0e1315c87198e4454135c31ae7811769ff383387
rishabh dixit <rishabhdixit@rishabhs-MacBook-Pro.local>
2017-07-12 11:34:18 +0800

    refactor: added encodedUrl in environment file and accessed from it

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
 	production: true,
-	api: 'https://welinktalent-server-new.herokuapp.com'
+	api: 'https://welinktalent-server-new.herokuapp.com',
+	encodedUrl: 'https%3A%2F%2Fwelinktalent-client.herokuapp.com'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,8 @@
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `angular-cli.json`.
 export const environment = {
+	// TODO remove heroku urls once prod env starts working
 	production: false,
-	api: 'https://welinktalent-server-new.herokuapp.com'
+	api: 'https://welinktalent-server-new.herokuapp.com' || 'http://localhost:8080',
+	encodedUrl: 'https%3A%2F%2Fwelinktalent-client.herokuapp.com' || 'http%3A%2F%2Flocalhost%3A4200'
 };
