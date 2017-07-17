23db6d9e1d4d05a9a4bb3466cae7adc733a7e036
Philip Andrew Pulgade <andrew.pulgade@gmail.com>
2017-07-17 13:11:50 +0800

    Feat: Created Admin Home Page

--- src/app/components/Admin/admin-create-job.component.ts
+++ src/app/components/Admin/admin-create-job.component.ts
@@ -1,21 +1,25 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
+import { Job } from '../../models/job.model';
 
 @Component({
 	selector: `app-admin-create-job`,
 	template: `
 		<form [formGroup]="createJobForm">
 			<div class="row">
+				<h2>Job Details: </h2>
+			</div>
+			<div class="row">
 				<div class="col-md-6">
 					<div class="form-group">
 						<label class="labelWeight">Job Tite*</label>
-						<input type="text" class="formControl" formControlName="title" required/>
+						<input type="text" class="form-control" formControlName="title" required/>
 					</div>
 				</div>
 				<div class="col-md-6">
 					<div class="form-group">
 						<label class="labelWeight">Industry*</label>
-						<input type="text" class="formControl" formControlName="industry" placeholder="Information Technology" required/>
+						<input type="text" class="form-control" formControlName="industry" placeholder="Information Technology" required/>
 					</div>
 				</div>
 			</div>
@@ -23,14 +27,111 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 				<div class="col-md-6">
 					<div class="form-group">
 						<label class="labelWeight">Job Type*</label>
-						<input type="text" class="formControl" formControlName="jobType" required/>
+						<input type="text" class="form-control" formControlName="jobType" placeholder="Permanent" required/>
 					</div>
 				</div>
 				<div class="col-md-6">
 					<div class="form-group">
 						<label class="labelWeight">Employment Type*</label>
-						<input type="text" class="formControl" formControlName="employmentType" required/>
+						<input type="text" class="form-control" formControlName="employmentType" placeholder="full-time" required/>
 					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-6">
+					<div class="form-group">
+						<label class="labelWeight">Expertise*</label>
+						<input type="text" class="form-control" formControlName="expertise" placeholder="Android Developer" required/>
+					</div>
+				</div>
+				<div class="col-md-6">
+					<div class="form-group">
+						<label class="labelWeight">Years of Experience*</label>
+						<input type="number" class="form-control" formControlName="experience" required/>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-6">
+					<div class="form-group">
+						<label class="labelWeight">Salary From*</label>
+						<input type="number" class="form-control" formControlName="salaryFrom" required/>
+					</div>
+				</div>
+				<div class="col-md-6">
+					<div class="form-group">
+						<label class="labelWeight">Salary To*</label>
+						<input type="number" class="form-control" formControlName="salaryTo" required/>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-6">
+					<div class="form-group">
+						<label class="labelWeight">Application Slots*</label>
+						<input type="number" class="form-control" formControlName="slots" required/>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<h2>Company Details:</h2>
+			</div>
+			<div class="row">
+				<div class="col-md-6">
+					<div class="form-group">
+						<label class="labelWeight">Company Name*</label>
+						<input type="text" class="form-control" formControlName="companyName" required/>
+					</div>
+				</div>
+				<div class="col-md-6">
+					<div class="form-group">
+						<label class="labelWeight">Location*</label>
+						<input type="text" class="form-control" formControlName="location" required/>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-6">
+					<div class="form-group">
+						<label class="labelWeight">Contact Name*</label>
+						<input type="text" class="form-control" formControlName="contactName" required/>
+					</div>
+				</div>
+				<div class="col-md-6">
+					<div class="form-group">
+						<label class="labelWeight">Contact Email*</label>
+						<input type="text" class="form-control" formControlName="contactEmail" required/>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-6">
+					<div class="form-group">
+						<label class="labelWeight">Phone Number*</label>
+						<input type="text" class="form-control" formControlName="phoneNumber" required/>
+					</div>
+				</div>
+				<div class="col-md-6">
+					<div class="form-group">
+						<label class="labelWeight">Company Logo*</label>
+						<input type="file" class="form-control" formControlName="logo" required/>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-12">
+					<div class="form-group">
+						<label class="labelWeight">About the Company*</label>
+						<textarea class="form-control" rows="4" formControlName="aboutCompany"></textarea>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-6">
+					<button class="btn btn-primary pull-right" (click)="onClick()">SAVE</button>
+				</div>
+				<div class="col-md-6">
+					<button class="btn btn-default pull-left">CANCEL</button>
 				</div>
 			</div>
 		</form>
@@ -44,8 +145,16 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 })
 
 export class AdminCreateJobComponent implements OnInit {
+	@Input() job: Job;
+	@Output() removeSkillEvent = new EventEmitter<any>();
+	@Output() editEvent = new EventEmitter<string>();
 
 	createJobForm: FormGroup;
+	isClicked:boolean = false;
+
+	get skills(): FormArray {
+		return <FormArray>this.createJobForm.get('skills');
+	}
 
 	constructor(public fb: FormBuilder) {	}
 
@@ -55,6 +164,58 @@ export class AdminCreateJobComponent implements OnInit {
 			industry: '',
 			jobType: '',
 			employmentType: '',
+			expertise: '',
+			experience: '',
+			salaryFrom: '',
+			salaryTo: '',
+			slots: '',
+			skills: this.fb.array(this.job.skills.map(() => this.skillsFormGroup())),
+			companyName: '',
+			location: '',
+			contactName: '',
+			contactEmail: '',
+			phoneNumber: '',
+			logo: null,
+			aboutCompany: '',
 		});
+	}
+
+	saveSkill(index) {
+		const profile = this.createJobForm.value;
+		// this.saveProfileEvent.emit(profile);
+	}
+
+	editMode(event, editId) {
+		this.editEvent.emit(editId);
+		event.preventDefault();
+	}
+
+	addSkill(event) {
+		this.skills.push(this.skillsFormGroup());
+		this.editMode(event, 'skill' + (this.skills.length - 1));
+	}
+
+	cancelSkill(event, index) {
+		const skill = this.job.skills[index];
+        //
+		// if (!skill || !skill.id) {
+		// 	this.skills.removeAt(index);
+		// }
+
+		this.editMode(event, '');
+	}
+
+	removeSkill(index) {
+		this.removeSkillEvent.emit({ index });
+	}
+
+	skillsFormGroup(): FormGroup {
+		return this.fb.group({
+			name: '',
+		});
+	}
+
+	onClick() {
+		console.log(this.createJobForm.value);
 	}
 }
