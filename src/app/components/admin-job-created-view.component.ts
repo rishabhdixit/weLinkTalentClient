a8662a4200931de1a2edae9e407ee2bc1e1af8d2
Philip Andrew Pulgade <andrew.pulgade@gmail.com>
2017-07-20 11:42:02 +0800

    Feat: Display Jobs Created

---
+++ src/app/components/admin-job-created-view.component.ts
@@ -0,0 +1,97 @@
+import { Component, Input } from '@angular/core';
+import { Store } from '@ngrx/store';
+import * as fromRoot from '../reducers';
+import { Job } from '../models/job.model';
+
+@Component({
+	selector: `app-admin-job-created-view`,
+	template: `
+		<div class="row">
+			<div class="col-md-7">
+				<div class="row">
+					<div class="col-md-12">
+						<h5 style="color: #57148D;">{{ job.title }}</h5>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-7">
+						<h6 style="color: #58595b;" *ngIf="job.company">{{ job.company.name }}</h6>
+					</div>
+					<div class="col-md-5 text-right">
+						<p>Posted {{ job.createdAt | date: 'yyyy-MM-dd' }}</p>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-12">
+						<p *ngIf="job.company" class="text-justify">{{ job.company.about }}</p>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-5">
+				<br/>
+				<div class="row">
+					<div class="col-md-1"></div>
+					<div class="col-md-10 tex-center">
+						<div class="text-center application-slots">
+							<h2 style="color: #FFF;">{{ job.remaining_slots }}</h2>
+							<h6 style="color: #FFF;">Application Slots Left</h6>
+						</div>
+					</div>
+					<div class="col-md-1"></div>
+				</div>
+				<br/>
+				<div class="row">
+					<div class="col-md-6">
+						<h5>Location:</h5>
+						<h5>Job type:</h5>
+						<h5>Emp type:</h5>
+						<h5>Salary from:</h5>
+						<h5>Salary to:</h5>
+					</div>
+					<div class="col-md-6">
+						<h5>{{job.location}}</h5>
+						<h5>Permanent</h5>
+						<h5>{{ job.employment_type }}</h5>
+						<h5>{{ job.salary_currency }}{{ job.salary_from }}</h5>
+						<h5>{{ job.salary_currency }}{{ job.salary_to }}</h5>
+					</div>
+				</div>
+				<br/>
+				<div class="row">
+					<div class="col-md-1"></div>
+					<div class="col-md-10">
+						<div class="read-more-link text-center">
+							<a style="color: #FFF;" routerLink="/jobs/{{ job._id }}"><h4>Read More</h4></a>
+						</div>
+					</div>
+					<div class="col-md-1"></div>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-12">
+				<hr/>
+			</div>
+		</div>
+	`,
+	styles: [`
+		.read-more-link {
+			width: 100%;
+			padding-top: 10px;
+			padding-bottom: 10px;
+			background: #57148D;
+		}
+		.application-slots {
+			width: 100%;
+			padding-top: 10px;
+			padding-bottom: 10px;
+			background: #58595b;
+		}
+	`],
+})
+
+export class AdminJobCreatedViewComponent {
+	@Input() job: Job;
+
+	constructor(private store: Store<fromRoot.State>) {}
+}
--- src/app/containers/Admin/admin-home-page.component.ts
+++ src/app/containers/Admin/admin-home-page.component.ts
@@ -1,17 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { Job } from '../../models/job.model';
+import * as fromRoot from '../../reducers';
+import * as JobAction from '../../actions/jobs.action';
+import { User } from '../../models/user.model';
+import { Store } from '@ngrx/store';
 
 @Component({
 	selector: 'app-admin-home-page',
 	template: `
 		<div class="container">
 			<div class="container-fluid">
-				<app-admin-home-view></app-admin-home-view>
+					<app-admin-home-view></app-admin-home-view>
+				<!--<div class="row">row-->
+					<app-admin-job-created-view *ngFor="let job of jobList$ | async | paginate: { itemsPerPage: 10, 
+																			currentPage: currentPage,
+																			totalItems: jobsTotalSize$ | async }"
+																			[job]="job">{{ job }}</app-admin-job-created-view>
+				<br/>
+				<pagination-controls (pageChange)="onChangePage($event)"></pagination-controls>
+				<!--</div>-->
 			</div>
 		</div>
 	`,
 	styles: [``],
 })
 
-export class AdminHomePageComponent {
-	constructor() {}
+export class AdminHomePageComponent implements OnInit {
+	jobList$: Observable<Job[]>;
+	jobsTotalSize$: Observable<number>;
+	currentPage = 1;
+	currentUser: User;
+
+	constructor(private store: Store<fromRoot.State>) {
+	}
+
+	ngOnInit() {
+		this.store.select(fromRoot.getUser).subscribe((user) => this.currentUser = user);
+		if (this.currentUser) {
+			this.store.dispatch(new JobAction.CreateJobsLoadAction(this.currentUser.id));
+			this.jobList$ = this.store.select(fromRoot.getAllJobsCreated);
+			this.jobsTotalSize$ = this.store.select(fromRoot.getTotalJobsCreated);
+		}
+	}
+
+	onChangePage(event) {
+		if (this.currentUser) {
+			this.currentPage = event;
+			this.store.dispatch(new JobAction.CreateJobsLoadAction(this.currentUser.id));
+		}
+	}
 }
