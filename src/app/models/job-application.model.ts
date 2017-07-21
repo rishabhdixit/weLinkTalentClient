16fbfe6a31b56815386e1f5c928a55880de6fc95
Jojie Palahang <jojiepalahang@Jojies-MacBook-Pro.local>
2017-07-21 12:42:44 +0800

    fix: for application form

--- src/app/models/job-application.model.ts
+++ src/app/models/job-application.model.ts
@@ -1,10 +1,19 @@
 export class JobApplication {
-		id: string;
-		user_id: string;
-		job_id: string;
-		files: any;
-		form_data: ApplicationDetails;
-		skills: any;
+	id: string;
+	user_id: string;
+	job_id: string;
+	files: any;
+	form_data: any;
+	skills: any;
+
+	// TODO - to be removed
+	reasonForLeaving: string;
+	strength: string;
+	improvements: string;
+	achievements: string;
+	management: string;
+	basePerMonth: number;
+	bonus: number;
 }
 
 export class ApplicationDetails {
