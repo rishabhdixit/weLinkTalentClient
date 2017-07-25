c01a6ca3ae9349086eed69edfabf12b38b517d1a
Jojie Palahang <jojiepalahang@Jojies-MacBook-Pro.local>
2017-07-25 18:13:48 +0800

    fix : ng serve --prod reported errors

---
+++ src/app/components/common/record-not-found-view.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'app-record-not-found-view',
+	template: ``,
+	styles: [``],
+})
+// TODO : expand this component soon.
+export class RecordNotFoundViewComponent {
+	constructor() {	}
+}
